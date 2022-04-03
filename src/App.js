e1b0e6f5226dffaf5f36b085d37745f081a48b22
Daria Vasilkova <vasilkov.darii@gmail.com>
2022-04-03 15:07:04 +0300

    deleting logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
-         <p> Edition <code>src/App.js</code> and save to reload.</p>
-        <h1>hello</h1>
+      <div className="Header">
+        <img src="./img/Group_101.png" alt="logo" />
+        <ul className="Menu">
+          <li>Home</li>
+          <li>Book</li>
+          <li>Join our team</li>
+        </ul>
+        <button className="signIn">Sign in</button>
+      </div>
+
+      <div className="Description">
+          <h1>always clean</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a
+            augue ac nibh facilisis eleifend. Fusce vitae lacinia est. Nulla et
+            congue elit. Orci varius natoque penatibus et magnis dis parturient
+            montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante
+            ipsum primis in faucibus. Pellentesque sodales nunc eu ultricies
+            euismod. Ut sed sem et sapien lacinia sollicitudin malesuada ut
+            nisl. Maecenas nec hendrerit justo.
+          </p>
+          <button className="book">Book now</button>
+        </div>
     </div>
   );
 }

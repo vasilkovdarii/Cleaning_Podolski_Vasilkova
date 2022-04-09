91d2fd67524841540613e777f33832d46a5fb606
Daria Vasilkova <vasilkov.darii@gmail.com>
2022-04-09 18:47:31 +0300

    adding image and styles to the main page

--- src/components/MainContent/MainPage.jsx
+++ src/components/MainContent/MainPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "../../componentStyles/MainPage.css";
 import Woman from "..//..//img/Group_239.png";
-import Background from "..//..//img/background.png";
 
 function MainPage() {
   return (
     <div className="main-page">
       <div className="text">
       <h2 style={{color:"#FFD836"}}>always clean</h2>
-      <h1 style={{color:"#6882EF"}}>HOME & OFFICE</h1>
+      <h1 style={{color:"#324975"}}>HOME & OFFICE</h1>
       <span style={{margin_top: "40px"}}>
         It’s hard to find someone who enjoys cleaning. It takes a lot of energy,
         time, and you always have to do it when there are so many other things
@@ -21,8 +20,9 @@ function MainPage() {
       <br />
       <button type="button" className="btn-book">Book now</button>
       </div>
-      {/* <img className="background" src={Background} alt="background" />
-      <img className="woman" src={Woman} alt="woman"/> */}
+      <div className="woman"> 
+      <img src={Woman} alt="background"/>
+      </div>
     </div>
   );
 }
